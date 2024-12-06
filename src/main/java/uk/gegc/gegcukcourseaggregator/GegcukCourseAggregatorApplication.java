c5e00dfb051e4f02df20ca20d7bba98904c71d92
Aleksei Lazunin <gegcuk@gmail.com>
2024-12-06 00:36:36 +0000

    Initial commit

---
+++ src/main/java/uk/gegc/gegcukcourseaggregator/GegcukCourseAggregatorApplication.java
@@ -0,0 +1,13 @@
+package uk.gegc.gegcukcourseaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GegcukCourseAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GegcukCourseAggregatorApplication.class, args);
+	}
+
+}
